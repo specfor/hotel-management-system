800e80716efb58352fffa4cf2848634422acaa8f
Manuja Ranathunga <manujamatheeshabc@gmail.com>
2025-10-17 16:27:14 +0530

    Added the paid_amount logic int the model layerfor the payment table insert operation

--- backend/src/models/paymentModel.ts
+++ backend/src/models/paymentModel.ts
@@ -1,5 +1,5 @@
 import { addNewPayment_repo } from "@src/repos/paymentRepo";
-import { checkBillExistByID_repo } from "@src/repos/finalBillRepo";
+import { checkBillExistByID_repo, getFinalBillByID_repo } from "@src/repos/finalBillRepo";
 import { PaymentPublic } from "@src/types/paymentTypes";
 
 // Get current date/time in PostgreSQL TIMESTAMP format
@@ -21,8 +21,28 @@ export async function addNewPayment_model(newPay: PaymentPublic) {
   if (await checkBillExistByID_repo(newPay.bill_id)) {
     const timestamp = getCurrentTimestamp();
     newPay.date_time = timestamp;
-    const x = await addNewPayment_repo(newPay);
-    return { success: true, payment_id: x };
+    const {paid_amount} = newPay;
+    const finalBill = await getFinalBillByID_repo(newPay.bill_id);
+    if (!finalBill || typeof finalBill.outstanding_amount !== "number") {
+      return { success: false, error: "Final bill not found or outstanding_amount missing" };
+    }
+    const { outstanding_amount } = finalBill;
+    if (outstanding_amount < paid_amount) {
+      const excess_amount = paid_amount - outstanding_amount;
+      const x = await addNewPayment_repo(newPay);
+      newPay.paid_amount = -excess_amount;
+      const y = await addNewPayment_repo(newPay);
+      return { success: true, payment_id: x };
+    }
+    if (outstanding_amount === paid_amount){
+      const x = await addNewPayment_repo(newPay);
+      return { success: true, payment_id: x };
+    }
+    if(outstanding_amount > paid_amount){
+      const x = await addNewPayment_repo(newPay);
+      return { success: true, payment_id: x };
+    }
+    return { success: false, error: "something went wrong" };
   } else {
     return { success: false, error: "Bill Id not found" };
   }
--- backend/src/repos/finalBillRepo.ts
+++ backend/src/repos/finalBillRepo.ts
@@ -1,4 +1,5 @@
 import db from "@src/common/util/db";
+import { FinalBillPublic } from "@src/types/finalBillTypes";
 
 export async function checkBillExistByID_repo(bill_id: number) {
   if (!db.isReady()) {
@@ -14,4 +15,24 @@ export async function checkBillExistByID_repo(bill_id: number) {
 
   const { rows } = await db.query(query, [bill_id]);
   return rows[0].record_exists as boolean;
+}
+
+export async function getFinalBillByID_repo(
+  bill_id: number,
+): Promise<FinalBillPublic | null> {
+  if (!db.isReady()) {
+    await db.connect();
+  }
+
+  const query = `
+    SELECT *
+    FROM final_bill
+    WHERE bill_id = $1
+  `;
+
+  const { rows } = await db.query(query, [bill_id]);
+  if (rows.length === 0) {
+    return null;
+  }
+  return rows[0] as FinalBillPublic;
 }
